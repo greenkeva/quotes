8a376b9037ae7544a1a6df1c4f403b5cb905d262
greenkeva <Shekeva.Green@ibm.com>
2022-01-10 00:14:29 -0600

    add JenkinsFile

---
+++ src/components/_tests/Home.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Home from '../home/Home';
+
+describe('renders without crashing', function () {
+    let mountedQuote;
+
+    beforeEach(()=>{
+        mountedQuote = shallow(<Home />);
+    })
+    
+    it('renders without crashing', function() {
+        const mountedQuote = shallow(<Home />);
+        expect(mountedQuote.length).toBeGreaterThan(0);
+    });
+
+    // it('renders a string', () => {
+    //     const quote = mountedQuote.find('h2').text();
+    //     expect(quote.length).toBeGreaterThan(0);
+    // });
+    
+});
