3e70eb7a060eb6106bfd851870de3f09bbb43dcd
greenkeva <Shekeva.Green@ibm.com>
2022-01-10 08:37:24 -0600

    update notfound.js

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 function NotFound() {
-    return <div color="white"> 404 Page Not Found </div>
+    return <div color="black"> 404 Page Not Found </div>
 }
 
 export default NotFound;
